64ea5eebebc88b603da575e977b6e3d2920b63a5
Parsec <sailcali@gmail.com>
2023-01-26 15:22:53 -0800

    Fixed the login script for new code

--- login.py
+++ login.py
@@ -1,14 +1,17 @@
 import pandas as pd
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
+from dotenv import load_dotenv, set_key, find_dotenv
 import os
 import base64
 import requests
 from datetime import datetime
 import pytz
 
-load_dotenv()
+DOTENV_FILE = find_dotenv()
+load_dotenv(DOTENV_FILE)
 
+SERVER_PWD = os.environ.get("API_PASSWORD")
+SERVER_IP = os.environ.get("SERVER_IP")
 CLIENT_ID = os.environ.get("ENPHASE_CLIENT_ID")
 CLIENT_SECRET = os.environ.get("ENPHASE_CLIENT_SECRET")
 client_string = f"{CLIENT_ID}:{CLIENT_SECRET}"
@@ -26,7 +29,9 @@ response = requests.post(ACCESS_URL + user_code, headers=header)
 body = response.json()
 access_token = body['access_token']
 refresh_token = body['refresh_token']
-data = [{'at': access_token, 'rt': refresh_token, 'acdate': datetime.now(pytz.UTC), 'rfdate': datetime.now(pytz.UTC), 'user': user_code}]
-df = pd.DataFrame(data)
-df.set_index(['user'], inplace=True)
-df.to_sql('sd_access', db, if_exists='replace')
+data = [{'at': access_token, 'rt': refresh_token, 'date': datetime.strftime(datetime.now(pytz.UTC), '%a, %d %b %Y %H:%M:%S %Z'), 'user': user_code}]
+response = requests.post("http://" + SERVER_IP + "/solar/access",
+                            headers={'password': SERVER_PWD},
+                            json=data)
+os.environ["ENPHASE_CODE"] = user_code
+set_key(DOTENV_FILE, "ENPHASE_CODE", os.environ["ENPHASE_CODE"])
