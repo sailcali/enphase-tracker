61d5e22bc202c7638333464d083f5cdd4dd91c81
Parsec <sailcali@gmail.com>
2023-05-09 15:38:04 -0700

    Added daily pool graphing

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from datetime import datetime, timedelta, date
 import time
 import requests
 from discordwebhook import Discord
+import psycopg2
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 from accessInstance import AccessInstance
 
@@ -16,11 +19,63 @@ load_dotenv()
 API_KEY = os.environ.get("ENPHASE_KEY")
 SYSTEM_ID = os.environ.get("SYSTEM_ID")
 CHANNEL_URL = os.getenv('ENPHASE_CHANNEL_URL')
+POOL_CHANNEL_URL = os.getenv('POOL_CHANNEL_URL')
 SERVER_IP = os.environ.get("SERVER_IP")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_HOST = os.environ.get("DB_HOST")
+DB_NAME = os.environ.get("DB_NAME")
 
 SYSTEM_URL = f"api.enphaseenergy.com/api/v4/systems/{SYSTEM_ID}/summary?key={API_KEY}"
 PRODUCTION_URL = f'https://api.enphaseenergy.com/api/v4/systems/{SYSTEM_ID}/telemetry/production_micro'
 
+def get_daily_pool_temp_graph():
+    # days_ago = 0
+    today = datetime.today().date()
+    # start_date = today - timedelta(days=days_ago)
+    start_date = today
+    end_date = start_date + timedelta(days=1)
+    query = f"""SELECT pooldata.datetime,
+        pooldata.roof_temp,
+        pooldata.water_temp,
+        pooldata.valve,
+        pooldata.temp_range
+    FROM pooldata
+    WHERE pooldata.datetime > '{start_date} 17:00:00+00'::timestamp with time zone AND pooldata.datetime < '{end_date} 02:00:00+00'::timestamp with time zone
+    ORDER BY pooldata.datetime DESC;"""
+
+    # query = "SELECT * from pool23apr"
+    # Establish connection to PostgreSQL database
+    with psycopg2.connect(
+            host=DB_HOST,
+            database=DB_NAME,
+            user=DB_USER,
+            password=DB_PASS) as conn:
+            
+            cur = conn.cursor()
+            cur.execute(query)
+
+
+            data = cur.fetchall()
+
+            # Close database connection
+            cur.close()
+
+    # Separate data into two lists for each column
+    column1_data = [row[1] for row in data]
+    column2_data = [row[2] for row in data]
+    times = [row[0] for row in data]
+
+    # Plot the data in one graph using Matplotlib
+    plt.plot(times, column1_data, label='Roof')
+    plt.plot(times, column2_data, label='Pool')
+    plt.xlabel('Time (UTC)')
+    plt.ylabel('Temp (F)')
+    plt.legend()
+    filename = "pool_today.jpg"
+    plt.savefig(filename)
+    discord = Discord(url=POOL_CHANNEL_URL)
+    discord.post(file={"Frame": open(filename, "rb"),})
 
 def get_production_data_from_select_day(current_date_timestamp):
     """Gather one day worth of production data from ENPHASE"""
@@ -44,6 +99,11 @@ def get_production_data_from_select_day(current_date_timestamp):
 
 
 if __name__ == '__main__':
+    try:
+        get_daily_pool_temp_graph()
+    except Exception:
+        discord = Discord(url=POOL_CHANNEL_URL)
+        discord.post(content=f"Pool data was either inaccessible or could not be compiled!\nError: {error}")
     try:
         # Create Access instance which manages the OAUTH2.0 keys and handles the db connection
         ACCESS_DATA = AccessInstance()
